31d40f8310b46c6b5523707370b4515f1ead811d
qBlackbirdp <blackbird9711@gmail.com>
2024-06-27 21:41:31 +0900

    3번 문제 피보나치 해결

--- src/main/java/com/koreait/Main.java
+++ src/main/java/com/koreait/Main.java
@@ -2,34 +2,21 @@ package com.koreait;
 
 import java.util.Scanner;
 
-//양의 정수 n을 요청받아 숫자 패턴의 피라미드를 출력하는 프로그램을 구현합니다. 피라미드는 n 레벨을 가져야 하며,
-//최상위 레벨에서 n부터 시작하여 맨 아래층에서 1까지 숫자가 감소해야 합니다.
+//3. 프로그램은 정수 10개의 피보나치 수열 숫자를 출력합니다. 수열을 생성하기 위해 루프를 사용합니다. (40점)
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("입력한 정수 높이의 피라미드를 만들거예요");
+        int x = 10;
 
-        System.out.print("입력할 정수 : ");
-        int x = sc.nextInt();
-        Fibonacci(x);
-        System.out.println();
+        for (int i = 0; i < x; i++) {
 
-        for (int i = 0; x >= i; x--) {
-            for (int j = i + 1; j < x; j++) {
-                System.out.print(j + " ");
-            }
-            if (x == 0) {
-                break;
-            }
-            System.out.println(x);
+            System.out.print(Fibonacci(i) + " ");
         }
     }
-
     public static int Fibonacci(int n) {
         if (n == 0) {
             return 0;
-        } else if (n == 1 || n == 2) {
+        } else if (n == 1) {
             return 1;
         }
         return Fibonacci(n - 1) + Fibonacci(n - 2);
