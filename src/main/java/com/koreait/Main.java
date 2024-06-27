c3ad3e96082a2efd2c082483e31d1cbe1855e1e2
qBlackbirdp <blackbird9711@gmail.com>
2024-06-27 14:56:21 +0900

    2번 문제 (제대로 해결 못함)

--- src/main/java/com/koreait/Main.java
+++ src/main/java/com/koreait/Main.java
@@ -2,32 +2,37 @@ package com.koreait;
 
 import java.util.Scanner;
 
-//1. 가로길이, 세로길이를 입력받아 사각형 넓이 만큼 별을 찍어내는 프로그램을 구현합니다(30점)
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//양의 정수 n을 요청받아 숫자 패턴의 피라미드를 출력하는 프로그램을 구현합니다. 피라미드는 n 레벨을 가져야 하며,
+//최상위 레벨에서 n부터 시작하여 맨 아래층에서 1까지 숫자가 감소해야 합니다.
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("별의 가로 길이를 입력해주세요. \n가로길이: ");
+        System.out.println("입력한 정수 높이의 피라미드를 만들거예요");
+
+        System.out.print("입력할 정수 : ");
         int x = sc.nextInt();
-        System.out.println("별의 가로길이 : " + x);
-
-        System.out.print("별의 세로 길이를 입력해주세요. \n세로길이: ");
-        int y = sc.nextInt();
-        System.out.println("별의 세로길이 : " + y);
-
+        Fibonacci(x);
         System.out.println();
 
-        System.out.println("가로길이 " + x + ", 세로길이 " + y + "의 사각형");
-
-        System.out.println();
-
-        for (int i = 0; i < y; i++) {
-            for (int j = 0; j < x; j++) {
-                System.out.print("*");
+        for (int i = 0; x >= i; x--) {
+            for (int j = i + 1; j < x; j++) {
+                System.out.print(j + " ");
             }
-            System.out.println();
+            if (x == 0) {
+                break;
+            }
+            System.out.println(x);
         }
+    }
 
+    public static int Fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1 || n == 2) {
+            return 1;
+        }
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
     }
 }
+
