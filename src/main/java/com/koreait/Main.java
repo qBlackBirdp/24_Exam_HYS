d9a96ea3df220f833387ce86adbf1ded450e7b1d
qBlackbirdp <blackbird9711@gmail.com>
2024-06-27 14:17:11 +0900

    1번 문제

--- src/main/java/com/koreait/Main.java
+++ src/main/java/com/koreait/Main.java
@@ -1,17 +1,33 @@
 package com.koreait;
 
+import java.util.Scanner;
+
+//1. 가로길이, 세로길이를 입력받아 사각형 넓이 만큼 별을 찍어내는 프로그램을 구현합니다(30점)
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("별의 가로 길이를 입력해주세요. \n가로길이: ");
+        int x = sc.nextInt();
+        System.out.println("별의 가로길이 : " + x);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.print("별의 세로 길이를 입력해주세요. \n세로길이: ");
+        int y = sc.nextInt();
+        System.out.println("별의 세로길이 : " + y);
+
+        System.out.println();
+
+        System.out.println("가로길이 " + x + ", 세로길이 " + y + "의 사각형");
+
+        System.out.println();
+
+        for (int i = 0; i < y; i++) {
+            for (int j = 0; j < x; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
+
     }
 }
